b1780c4011804bb45293abf76a25a69f7a8a26de
Kinga Szayer <kinga.szayer@iths.se>
2022-02-23 10:07:31 +0100

    Add axios and getItemFromId and getItemsFromCategory api calls, and currentProduct and currentCategoryProducts to state and single product view get data from api

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import * as API from  '@/api/index.js'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentProduct: null,
+    currentCategoryProducts: []
   },
   mutations: {
   },
   actions: {
+    async fetchItemFromId(context, itemId) {
+      const response = await API.getItemFromId(itemId)
+      this.state.currentProduct = response.data.post
+    },
+    async fetchItemsFromCategory(context, category) {
+      const response = await API.getItemsFromCategory(category)
+      console.log(response);
+    }
   },
   modules: {
   }

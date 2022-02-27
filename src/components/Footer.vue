9e3ca707e60b23f362c7cf179833e02a3bfd9e04
Kinga Szayer <kinga.szayer@iths.se>
2022-02-27 22:23:49 +0100

    Add router Path for Single Product view, add product to cart, add product description and price

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,36 +4,39 @@
       <div>
         <h3>Sinus</h3>
         <ul>
-            <li>Om oss</li>
-            <li>Bli medlem</li>
-            <li>Medlemskapsvillkor</li>
-            <li>Mina sidor</li>
+            <li> <a href="">Om oss</a></li>
+            <li> <a href="">Bli medlem</a></li>
+            <li> <a href="">Medlemskapsvillkor</a></li>
+            <li> <a href="">Mina sidor</a></li>
         </ul>
       </div>
       <div>
         <h3>Information</h3>
         <ul>
-            <li>Hållbarhet</li>
-            <li>Just nu</li>
-            <li>Tjänster & service</li>
-            <li>Magasin</li>
+            <li> <a href="">Hållbarhet</a></li>
+            <li> <a href="">Just nu</a></li>
+            <li> <a href="">Tjänster & service</a></li>
+            <li> <a href="">Magasin</a></li>
         </ul>
       </div>
       <div>
         <h3>Hjälp</h3>
         <ul>
-            <li>Kontakt & kundservice</li>
-            <li>Köpvillkor</li>
-            <li>Presentkort</li>
-            <li>Cookies</li>
+            <li> <a href="">Kontakt & kundservice</a></li>
+            <li> <a href="">Köpvillkor</a></li>
+            <li> <a href="">Presentkort</a></li>
+            <li> <a href="">Cookies</a></li>
         </ul>
       </div>
       <div>
         <h3>Följ oss</h3>
         <div class="media-icons-position">
-            <img class="media-icon-size" src="../assets/instagram-icon.png" alt="Instagram">
-            <img class="media-icon-size" src="../assets/facebook-icon.png" alt="Facebook">
-            <img class="media-icon-size" src="../assets/youtube-icon.png" alt="Play">
+            <a href="https://www.instagram.com" target="_blank">
+                <img class="media-icon-size" src="../assets/instagram-icon.png" alt="Instagram"></a>
+            <a href="https://www.facebook.com" target="_blank">
+                <img class="media-icon-size" src="../assets/facebook-icon.png" alt="Facebook"></a>
+            <a href="https://www.youtube.com" target="_blank">
+                <img class="media-icon-size" src="../assets/youtube-icon.png" alt="Play"></a>
         </div>
       </div>
   </div>

1779591d1d4befc41f6fd476fd6d6a50370d9bfe
mammutakademin <edithtrinitaria@gmail.com>
2022-03-01 16:11:32 +0100

    Cart feature

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
+ 
     <div id="nav">
       <router-link to="/">Home</router-link> | 
       <router-link to="/about">About</router-link> | 
-      <router-link to="/ProductPage">ProdctPage</router-link> | 
+      <router-link to="/ProductPage">ProductPage</router-link> | 
       <router-link to="/SingleProduct">SingleProduct</router-link> | 
       <router-link to="/Checkout">Checkout</router-link> | 
       <router-link to="/Cart">Cart</router-link>
@@ -11,7 +12,14 @@
     <router-view/>
   </div>
 </template>
+<script>
 
+export default {
+  data() {
+    
+  },
+}
+</script>
 <style lang="scss">
 *{
   margin: 0;
